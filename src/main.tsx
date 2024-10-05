8168564565271ac19b6950939be4b2aa50e7719d
AlexandrKataev <dkeyman@mail.ru>
2024-10-05 23:32:00 +0300

    добавил тему antd

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,37 @@ import { store } from "@app/store";
 import { Provider } from "react-redux";
 import { ConfigProvider } from "antd";
 
+const redColor = "#EF233C";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConfigProvider locale={ru_RU}>
+    <ConfigProvider
+      locale={ru_RU}
+      theme={{
+        components: {
+          Input: {
+            colorPrimary: "yellow",
+            colorError: redColor,
+            colorBorder: "yellow",
+            borderRadius: 10,
+            fontSize: 12,
+          },
+          DatePicker: {
+            borderRadius: 10,
+            fontSize: 14,
+          },
+          Dropdown: {
+            colorBgBase: "red",
+            colorBgContainer: "red",
+          },
+          Select: {
+            borderRadius: 10,
+            fontSize: 14,
+          },
+          Form: { colorError: redColor },
+        },
+      }}
+    >
       <Provider store={store}>
         <App />
       </Provider>
