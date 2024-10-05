fc40f6e1f2f0867265bd11a1a98974a3852f93a3
AlexandrKataev <dkeyman@mail.ru>
2024-10-05 19:17:30 +0300

    добавил апи автозаполнения имени

---
+++ src/shared/api/autocomplete.api.ts
@@ -0,0 +1,28 @@
+import { api } from "./base-api/base.api";
+
+interface NameAutocompleteOption {
+  value: string;
+  unrestricted_value: string;
+  data: {
+    surname: null | string;
+    name: null | string;
+    patronymic: null | string;
+    gender: null | "MALE" | "FEMALE";
+  };
+}
+
+// запросы для работы с автозаполнением (dadata.ru)
+export const autocompleteApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getNameSuggestions: builder.query<NameAutocompleteOption[], string>({
+      query: (name) => ({
+        url: `http://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio`,
+        body: { query: name },
+        method: "POST",
+      }),
+      transformResponse: (res) => res.suggestions,
+    }),
+  }),
+});
+
+export const { useGetNameSuggestionsQuery } = autocompleteApi;
